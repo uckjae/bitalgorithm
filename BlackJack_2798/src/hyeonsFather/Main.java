fb82bdfdc2812863a0a9416c7032a6b9b855af88
uckjae.lee <uckjae@naver.com>
2019-12-03 17:53:02 +0900

    일단 끝

--- BlackJack_2798/src/hyeonsFather/Main.java
+++ BlackJack_2798/src/hyeonsFather/Main.java
@@ -1,5 +1,40 @@
 package hyeonsFather;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
+public class Main {
+	
+	public static int blackJack(int[] cards,int m) {
+		int maxSum = 0;
+		
+		for(int i=0; i<=cards.length-3; i++) {
+			for(int j=i+1; j<=cards.length-2;j++) {
+				for(int k=j+1; k<=cards.length-1;k++) {
+					int sum = cards[i] + cards[j] + cards[k];
+					maxSum = (sum<=m && sum>maxSum) ? sum : maxSum;
+				}
+			}
+		}
+		
+		return maxSum;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		int[] firstLine = new int[2];
+		firstLine = Arrays.stream(br.readLine().trim().split(" ")).mapToInt(Integer::parseInt).toArray();
+		
+		int[] cards = new int[firstLine[0]];
+		
+		cards = Arrays.stream(br.readLine().trim().split(" ")).mapToInt(Integer::parseInt).toArray();
+		
+		int answer = blackJack(cards, firstLine[1]);
+		
+		System.out.println(answer);
+	}
 }
