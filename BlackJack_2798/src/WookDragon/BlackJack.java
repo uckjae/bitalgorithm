8b2ac128d38bd5d830b003159dd681cc22c47637
wook_dragon <wook_dragon@kakao.com>
2019-12-09 19:09:27 +0900

    1209wookdraogn

---
+++ BlackJack_2798/src/WookDragon/BlackJack.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class BlackJack {
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int count = sc.nextInt();
+      int twentyone = sc.nextInt();
+      int hap =0;
+      ArrayList<Integer> temp = new ArrayList<Integer>();
+      int[] num = new int[count];
+      for(int i=0;i<num.length;i++) {
+         num[i]=sc.nextInt();
+         hap+=num[i];
+      }
+      for(int i=0;i<num.length-1;i++) {
+         for(int j=i+1;j<num.length;j++) {
+            if(hap-(num[i]+num[j])<=twentyone) {
+               temp.add(hap-(num[i]+num[j]));
+            }
+         }
+      }
+      temp.sort(Comparator.reverseOrder());
+      System.out.println(temp.get(0));
+   }
+}
